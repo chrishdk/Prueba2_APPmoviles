f8502df1ac0bf48fb8c7a51da9d62a6cac476ba1
Marcelo Morales <80928132+Marcel0M@users.noreply.github.com>
2021-11-06 03:22:53 -0300

    creacion de api.service y interfaces

--- Prueba2_APPmoviles/src/app/app.module.ts
+++ Prueba2_APPmoviles/src/app/app.module.ts
@@ -9,10 +9,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { SQLite } from '@ionic-native/sqlite/ngx';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SQLite],
   bootstrap: [AppComponent],
 })
